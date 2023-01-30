bba5f29e1b9d0fcc9da5b0618bda5e63d8b956a2
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-01-30 05:51:40 +0400

    Helper class added

---
+++ UkadTask/Helper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkadTask
+{
+    public static class Helper
+    {
+        public static bool UrlExists(string url)
+        {
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    using (client.OpenRead(url))
+                    {
+                        return true;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public static string ConvertUrlToSitemap(string url)
+        {
+            return $"{url}/sitemap.xml";
+        }
+        public static int GetResponseTime(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            response.Close();
+
+            timer.Stop();
+
+            return timer.Elapsed.Milliseconds;
+        }
+        public static void PrintOrderedByTime(List<Url> list)
+        {
+            foreach (var item in list.OrderBy(x => x.ElapsedTime))
+            {
+                Console.WriteLine($"{item.UrlName}, Response time(ms): {item.ElapsedTime}");
+            }
+            Console.WriteLine("\n");
+        }
+        public static void Print(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
--- UkadTask/Program.cs
+++ UkadTask/Program.cs
@@ -8,75 +8,75 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Http;
 using System.Reflection;
 using System.Linq;
+using System.Xml.Linq;
 
-Console.Write("Please enter url address: ");
-string urlName = Console.ReadLine();
-
-Stopwatch stopwatch = new Stopwatch();
 List<Url> urlList = new List<Url>();
-
-XmlDocument xmlDoc = new XmlDocument();
 List<Url> xmlList = new List<Url>();
 
-//Load relative path
-var fileName = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"Sitemap.xml");
-xmlDoc.Load(fileName);
+//Console.Write("Please enter url address: ");
+//string urlName = Console.ReadLine();
 
-XmlNodeList xmlNodes = xmlDoc.SelectNodes("/root/url");
-
-foreach (XmlNode node in xmlNodes)
-{
-    stopwatch.Start();
-    if (node.InnerText.Contains("/"))
-    {
-        stopwatch.Start();
-        xmlList.Add(new Url { UrlName = node.InnerText.Replace("\n", "").Trim(), ElapsedTime = stopwatch.ElapsedTicks });
-    }    
-}
-Console.WriteLine($"Urls found in Sitemap.mxl: {xmlList.Count}");
+//string urlName = "https://jwt.io/";
+string urlName = "https://lenta.ru/";
+//string urlName = "https://www.google.com/";
 
 HtmlWeb web = new HtmlWeb();
-HtmlDocument htmlDoc = web.Load(urlName);
-
-//Use the SelectNodes method to find all the "a" elements in the document:
-HtmlNodeCollection htmlNodes = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
-
-foreach (HtmlNode node in htmlNodes)
+if (Helper.UrlExists(urlName))
 {
-    stopwatch.Start();    
-    if (node.OuterHtml.Contains("/"))
+    HtmlDocument htmlDoc = web.Load(urlName);    
+
+    //Use the SelectNodes method to find all the "a" elements in the document:
+    HtmlNodeCollection htmlNodes = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
+
+    foreach (HtmlNode node in htmlNodes)
     {
-        stopwatch.Stop();
-        urlList.Add(new Url { UrlName = node.Attributes["href"].Value.TrimEnd('/'), ElapsedTime = stopwatch.ElapsedTicks });
+        if (node.OuterHtml.Contains("/"))
+        {
+            urlList.Add(new Url { UrlName = node.Attributes["href"].Value.TrimEnd('/'), ElapsedTime = Helper.GetResponseTime(urlName) });
+        }
     }
 }
+else
+{
+    Console.WriteLine("Url does not exist");
+    return;
+}
+
+XmlDocument xmlDoc = new XmlDocument();
+
+string sitemapUrl = Helper.ConvertUrlToSitemap(urlName);
+if (Helper.UrlExists(sitemapUrl))
+{
+    xmlDoc.Load(sitemapUrl);
+
+    foreach (XmlNode node in xmlDoc.DocumentElement)
+    {
+        //add urls located in <loc> node to xmlList
+        xmlList.Add(new Url { UrlName = node.FirstChild.InnerXml, ElapsedTime = Helper.GetResponseTime(sitemapUrl) });
+    }
+}
+else
+{
+    Console.WriteLine("Sitemap.xml does not exist");
+}
+
 Console.WriteLine($"Urls(html documents) found after crawling a website: {urlList.Count}");
+Console.WriteLine($"Urls found in sitemap.xml: {xmlList.Count}");
 
 var xmlListOnlyName = xmlList.Select(x => x.UrlName);
 var urlListOnlyName = urlList.Select(x => x.UrlName);
 
-Console.WriteLine();
-Console.WriteLine("1. Merge ordered by timing");
-var totalList = xmlList.Concat(urlList).OrderBy(i => i.ElapsedTime);
-
-foreach (var item in totalList)
-{
-    Console.WriteLine($"{item.UrlName} {item.ElapsedTime}");
-}
+Console.WriteLine("\n1. Merge ordered by timing");
+var totalList = xmlList.Concat(urlList).ToList();
+Helper.PrintOrderedByTime(totalList);
 
 Console.WriteLine();
-Console.WriteLine("2. Except URL");
-var exceptXml = xmlListOnlyName.Except(urlListOnlyName);
-foreach (var item in exceptXml)
-{
-    Console.WriteLine(item);
-}
+Console.WriteLine("\n2. Urls founded in sitemap.xml but not founded after crawling a web site:");
+var exceptXml = xmlListOnlyName.Except(urlListOnlyName).ToList();
+Helper.Print(exceptXml);
 
 Console.WriteLine();
-Console.WriteLine("3. Except Sitemap.xml");
-var exceptUrl = urlListOnlyName.Except(xmlListOnlyName);
-foreach (var item in exceptUrl)
-{
-    Console.WriteLine(item);
-}
+Console.WriteLine("\n3. Urls founded by the crawling the website but not in sitemap.xml");
+var exceptUrl = urlListOnlyName.Except(xmlListOnlyName).ToList();
+Helper.Print(exceptUrl);
 
